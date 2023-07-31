170485059bbe1245a6645b0a5b4dff93dd4c1aa6
Fan Yang <yangfanhewangzheng@gmail.com>
2023-07-31 12:25:46 -0400

    try to submiit

--- Classes/week4/DOMLAB/script.js
+++ Classes/week4/DOMLAB/script.js
@@ -6,21 +6,6 @@ var menuLinks = [
     {text: 'account', href: '/account'},
   ];
   
-// for (let i= 0; i< menuLinks.length; i++ ){
-//   let a = document.createElement('a');
-//   a.textContent = menuLinks[i].text;
-//   a.setAttribute('href', menuLinks[i].href);
-//   topMenuEl.appendChild(a);
-// }
-
-//   Iterate over the entire menuLinksarray and for each "link" object:
-
-// Create an <a>element.
-// On the new element, add an hrefattribute with its value set to the hrefproperty of the "link" object.
-// Set the new element's content to the value of the textproperty of the "link" object.
-// Append the new element to the topMenuElelement.
-
-
 const mainEl = document.querySelector("main");
 mainEl.style.backgroundColor = "#4a4e4d";
 mainEl.classList.add("flex-ctr");
@@ -29,8 +14,9 @@ const topMenuEl = document.querySelector("#top-menu");
 topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = "#0e9aa7";
 topMenuEl.classList.add("flex-around");
+
 for(let i = 0; i < menuLinks.length; i++){
-    let a = document.createElement('a')
+    let a = document.createElement("a")
     a.setAttribute("href", menuLinks[i].href)
     a.textContent = menuLinks[i].text
     topMenuEl.appendChild(a)
